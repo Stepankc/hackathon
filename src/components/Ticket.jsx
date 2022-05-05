1642645c0d65217dc467d8e68d0a1f04e928e69d
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2022-05-06 00:48:49 +0500

    123

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const ticket = (props) => {
     return (
         <div>
-            {console.log(props.post.id)}
             <div className='ticket'>
                 <div className='ticketName'>{props.post.id}</div>
                 <div className='ticketStatus done'>DONE</div>
---
+++ src/pages/DocumentPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const DocumentPage = () => {
+    return (
+        <div>
+            <div className='refactorDoc'>
+                <div className="info">12313</div>
+                <div className='document'>
+                    <embed src="https://africau.edu/images/default/sample.pdf" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DocumentPage;
