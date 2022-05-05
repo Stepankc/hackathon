1d103b4f28970f74c342af0a496eb38eb2b1f724
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2022-05-06 03:11:04 +0500

    НАКОНЕЦТО СУКСА БЛЯТЬ АААААААААА ЭТО СДЕЛАНО АПЗЙ

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 const ticket = (props) => {
+
     return (
         <div>
+            <Link to="/document">
             <div className='ticket'>
                 <div className='ticketName'>{props.post.id}</div>
                 <div className='ticketStatus done'>DONE</div>
             </div>
             <hr className='underline' />
+            </Link>
         </div>
     )
 }
