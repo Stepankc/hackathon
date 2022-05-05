dead89fcddca69d809d94ba79fd3dd6a50a24e27
i21s692 <i21s692@students.it-college.ru>
2022-05-05 11:20:01 +0500

    add ticket

---
+++ src/components/Ticket.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ticket = () => {
+    return (
+        <div className='ticket'>
+            <div className='ticketName'>qwerty.pdf</div>
+            <div className='ticketStatus done'>DONE</div>
+        </div>
+    )
+}
+
+export default ticket
