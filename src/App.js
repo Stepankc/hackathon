9e64ddcceaa9c3a3998cd458e6ade7860a97f874
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2022-05-05 11:16:48 +0600

    added mui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
-import "./components/LoginPage.css";
+import "./components/LoginPage.sass";
 import Loginpage from "./components/loginpage";
 import "./style/app.sass";
 
