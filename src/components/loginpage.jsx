c9eb92fd855c2892249764555755823f7b5bb104
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2022-05-05 11:16:26 +0600

    Added MUI

--- src/components/loginpage.jsx
+++ src/components/loginpage.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Loginpage = () => {
-    return (
-        <div className="LoginPage">
-            <div className="LoginBlock">
-                <h1 className="LoginTitle">Вход</h1>
-                <div className='component'>
-                    <h2 className="LoginText">Логин:</h2>
-                    <input type="text" className="LoginInput" />
-                </div>
-                <div className='component'>
-                <h2 className="LoginText">Пароль:</h2>
-                <input type="password" className="LoginInput" />
-                </div>
-                <button className="LoginButton">sing in</button>
-            </div>
+  return (
+    <div className="LoginPage">
+      <div className="LoginBlock">
+        <h1 className="LoginTitle">Вход</h1>
+        <div className="component">
+          <h2 className="LoginText">Логин:</h2>
+          <input type="text" className="LoginInput" />
         </div>
-    );
+        <div className="component">
+          <h2 className="LoginText">Пароль:</h2>
+          <input type="password" className="LoginInput" />
+        </div>
+        <button className="LoginButton">sing in</button>
+      </div>
+    </div>
+  );
 };
 
 export default Loginpage;
